53e0bbd53271bfd74ae2c42496e65b5639f3052d
JustinAMB <Justinmb@outlook.es>
2022-06-09 21:42:59 -0600

    se crea la clase cantidad

---
+++ Models/Hora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversorHoras.Models
+{
+    public class Hora
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+    }
+}
